7364884ce3219a24c6798fe5c0b219ed5113ab06
alpachinof <m.chahkandi2@gmail.com>
2023-02-18 16:44:09 +0330

    finished frontstore

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        inter: ["Inter", "Avenir", "Helvetica", "Arial", "sans-serif"],
+        sans: ["IranYekan", "Avenir", "Helvetica", "Arial", "sans-serif"],
+        yekan: ['IranYekan'],
       },
       dropShadow: {
         logo: "0 0 2em #646cffaa",
